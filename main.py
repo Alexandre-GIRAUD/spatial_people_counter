3fb1905b91ab902363d052210eb4869cf0f8689b
unknown <alexandre.giraud18@gmail.com>
2021-12-19 18:45:24 +0100

    Add bounding boxes

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from utils.load_images import load_image_into_numpy_array
 import matplotlib.pyplot as plt
 from utils.parse_image import parse_image
 from utils.create_labelmap import create_labelmap
-from utils.parse_model_output import class_counter
-
+from utils.parse_model_output import class_counter, draw_boxes
+import matplotlib.image 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--threshold", help="threshold for validatiing a model prediction",default=0.8)
@@ -14,17 +14,29 @@ args = parser.parse_args()
 
 #GLOBAL VARIABLES
 IMG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
-
+img_name = "president1.jpg"
 #detector = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1")
 detector = tf.keras.models.load_model(r"C:\Users\Giraud-Alexandre\Desktop\Object_Detection_For_Data_Centers\Spatial_people_counter\centernet_resnet101v1_fpn_512x512_1.tar\centernet_resnet101v1_fpn_512x512_1")
-image_tensor = load_image_into_numpy_array(os.path.join(IMG_PATH,"president1.jpg"))
+image_array = load_image_into_numpy_array(os.path.join(IMG_PATH,img_name))
+print(image_array.shape)
 
 #Prediction
-detector_output = detector(image_tensor)
+detector_output = detector(image_array)
 
 #Parsed image
 label_map = create_labelmap("mscoco_label_map.pbtxt")
 
 logs = class_counter(detector_output,label_map,args.threshold)
 
-print("Sur cette photo il y'a {} personnes".format(logs["person"]))
+try:
+    nb_person = logs["person"]
+except:
+    nb_person=0
+
+print("Sur cette photo il y'a {} personnes".format(nb_person))
+
+#Draw boxes
+image_with_boxes = draw_boxes(detector_output,image_array,label_map,args.threshold)
+plt.figure()
+plt.imshow(image_with_boxes[0])
+matplotlib.image.imsave(f"outputs/{img_name[:-4]}.png",image_with_boxes[0])
--- utils/parse_model_output.py
+++ utils/parse_model_output.py
@@ -2,7 +2,7 @@
 def class_counter(model_output,label_map,threshold):
     """
     Write a log based on the CNN's output
-    Input:
+    Inputs:
         model_output, label_map
     Output:
         dict(class:nb_detected)
@@ -16,5 +16,26 @@ def class_counter(model_output,label_map,threshold):
             logs[label_map[c]["name"]]=1
     return logs
 
-
-        
+def draw_boxes(model_output,image,label_map,threshold):
+    """
+    Take an image as input and draw the box over the detected objects
+    Inputs:
+        model_output,label_map,image,threshold
+    Outputs:
+        image with the bounding box
+    """
+    image_with_box = image.copy()
+    height = image_with_box.shape[1]
+    width = image_with_box.shape[2]
+    detected_class = model_output["detection_classes"][model_output["detection_scores"]>threshold].numpy()
+    boxes = model_output["detection_boxes"][model_output["detection_scores"]>threshold].numpy()
+    for i in range(len(detected_class)):
+        name = label_map[detected_class[i]]["name"]
+        #draw box
+        ymin,xmin,ymax,xmax = boxes[i]
+        ymin,xmin,ymax,xmax = int(ymin*height),int(xmin*width),int(ymax*height),int(xmax*width)
+        image_with_box[0,max(0,ymin-2):ymin+2,xmin:xmax,:] = 0
+        image_with_box[0,ymax-2:min(ymax+2,height),xmin:xmax,:] = 0
+        image_with_box[0,ymin:ymax,max(0,xmin-2):xmin+2,:] = 0
+        image_with_box[0,ymin:ymax,xmax-2:min(xmax+2,width),:] = 0
+    return image_with_box
