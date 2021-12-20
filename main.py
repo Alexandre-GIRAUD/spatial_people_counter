49b55f3c740f72a8c771c330cfd3bf924427c5c0
unknown <alexandre.giraud18@gmail.com>
2021-12-20 11:25:01 +0100

    add class and proba in bbox

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils.load_images import load_image_into_numpy_array
 import matplotlib.pyplot as plt
 from utils.parse_image import parse_image
 from utils.create_labelmap import create_labelmap
-from utils.parse_model_output import class_counter, draw_boxes
+from utils.parse_model_output import class_counter, draw_boxes ,save_image_with_boxes_and_classes
 import matplotlib.image 
 import argparse
 parser = argparse.ArgumentParser()
@@ -14,12 +14,10 @@ args = parser.parse_args()
 
 #GLOBAL VARIABLES
 IMG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
-img_name = "president1.jpg"
+img_name = "rue1.jpg"
 #detector = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1")
 detector = tf.keras.models.load_model(r"C:\Users\Giraud-Alexandre\Desktop\Object_Detection_For_Data_Centers\Spatial_people_counter\centernet_resnet101v1_fpn_512x512_1.tar\centernet_resnet101v1_fpn_512x512_1")
 image_array = load_image_into_numpy_array(os.path.join(IMG_PATH,img_name))
-print(image_array.shape)
-
 #Prediction
 detector_output = detector(image_array)
 
@@ -37,6 +35,5 @@ print("Sur cette photo il y'a {} personnes".format(nb_person))
 
 #Draw boxes
 image_with_boxes = draw_boxes(detector_output,image_array,label_map,args.threshold)
-plt.figure()
-plt.imshow(image_with_boxes[0])
-matplotlib.image.imsave(f"outputs/{img_name[:-4]}.png",image_with_boxes[0])
+
+save_image_with_boxes_and_classes(detector_output,image_with_boxes,label_map,args.threshold, path=f"outputs/{img_name[:-4]}.png")
