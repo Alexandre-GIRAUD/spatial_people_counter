7a65207a54c722482a5d03c6297685f5d5f089fd
unknown <alexandre.giraud18@gmail.com>
2021-12-20 20:13:58 +0100

    rewrite main

--- main.py
+++ main.py
@@ -7,33 +7,48 @@ from utils.parse_image import parse_image
 from utils.create_labelmap import create_labelmap
 from utils.parse_model_output import class_counter, draw_boxes ,save_image_with_boxes_and_classes
 import matplotlib.image 
+from datetime import date, datetime
 import argparse
+
+
 parser = argparse.ArgumentParser()
+parser.add_argument("-i","--image",help="path of input of the neuron",required=True)
 parser.add_argument("-t","--threshold", help="threshold for validatiing a model prediction",default=0.8)
 args = parser.parse_args()
 
-#GLOBAL VARIABLES
-IMG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
-img_name = "rue1.jpg"
-#detector = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1")
-detector = tf.keras.models.load_model(r"C:\Users\Giraud-Alexandre\Desktop\Object_Detection_For_Data_Centers\Spatial_people_counter\centernet_resnet101v1_fpn_512x512_1.tar\centernet_resnet101v1_fpn_512x512_1")
-image_array = load_image_into_numpy_array(os.path.join(IMG_PATH,img_name))
-#Prediction
-detector_output = detector(image_array)
 
-#Parsed image
-label_map = create_labelmap("mscoco_label_map.pbtxt")
+def prediction(path_image,t):
+    #GLOBAL VARIABLES
 
-logs = class_counter(detector_output,label_map,args.threshold)
+    #detector = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1")
+    detector = tf.keras.models.load_model(r"C:\Users\Giraud-Alexandre\Desktop\Object_Detection_For_Data_Centers\Spatial_people_counter\centernet_resnet101v1_fpn_512x512_1.tar\centernet_resnet101v1_fpn_512x512_1")
+    image_array = load_image_into_numpy_array(path_image)
+    #Prediction
+    detector_output = detector(image_array)
 
-try:
-    nb_person = logs["person"]
-except:
-    nb_person=0
+    #Parsed image
+    label_map = create_labelmap("mscoco_label_map.pbtxt")
 
-print("Sur cette photo il y'a {} personnes".format(nb_person))
+    logs = class_counter(detector_output,label_map,t)
 
-#Draw boxes
-image_with_boxes = draw_boxes(detector_output,image_array,label_map,args.threshold)
+    try:
+        nb_person = logs["person"]
+    except:
+        nb_person=0
+    today = date.today()
+    day = today.strftime("%b-%d-%Y")
+    
+    with open(f"logs/logs_{day}.txt",'w+') as f:
+        now = datetime.now()
+        h = now.strftime("%d/%m/%Y %H:%M:%S")
+        f.write("\n")
+        f.write(h + f" : {nb_person} personnes")
+        
 
-save_image_with_boxes_and_classes(detector_output,image_with_boxes,label_map,args.threshold, path=f"outputs/{img_name[:-4]}.png")
+    #Draw boxes
+    image_with_boxes = draw_boxes(detector_output,image_array,label_map,t)
+
+    save_image_with_boxes_and_classes(detector_output,image_with_boxes,label_map,t, path=f"outputs/" + path_image)
+
+if __name__=="__main__":
+    prediction(args.image,args.threshold)
